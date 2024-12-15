89aea2e18c4f21e0422bf0dad557746d6a700187
Jeral Pineda <jeral.cris@gmail.com>
2024-12-15 15:04:08 -0600

    fix: Zustand - Gestor de estado

---
+++ presentation/store/usePermissions.ts
@@ -0,0 +1,30 @@
+import { create } from "zustand";
+
+import { PermissionStatus } from "@/infrastructure/interfaces/location";
+import {
+  checkLocationPermissions,
+  requestLocationPermissions,
+} from "@/core/actions/permissions/location";
+
+interface PermissionsState {
+  locationStatus: PermissionStatus;
+
+  requestLocationPermissions: () => Promise<PermissionStatus>;
+  checkLocationPermissions: () => Promise<PermissionStatus>;
+}
+
+export const usePermissions = create<PermissionsState>((set) => ({
+  locationStatus: PermissionStatus.CHECKING,
+
+  requestLocationPermissions: async () => {
+    const status = await requestLocationPermissions();
+    set({ locationStatus: status });
+    return status;
+  },
+
+  checkLocationPermissions: async () => {
+    const status = await checkLocationPermissions();
+    set({ locationStatus: status });
+    return status;
+  },
+}));
