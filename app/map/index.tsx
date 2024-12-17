ba102d2bb0e2c3adb73031d382a080d53aa4d1d1
Jeral Pineda <jeral.cris@gmail.com>
2024-12-16 20:58:38 -0600

    fix: Expo MapView

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+
+// TODO: Expo Maps no esta disponible para la nueva arquitectura de Expo, esperar a que se implementen los nuevos componentes de expo
 
 export default function Map() {
   return (
-    <View>
-      <Text>Map</Text>
+    <View style={styles.container}>
+      <View style={styles.map} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
